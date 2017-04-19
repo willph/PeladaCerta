b755f31ceb4ce214fa8886a70e7e2763e8217569
rafael@roadmaps.com.br <rafael@roadmaps.com.br>
2017-04-18 21:24:40 -0300

    mmm

--- app/src/main/java/com/example/will/peladacerta/LoginActivity.java
+++ app/src/main/java/com/example/will/peladacerta/LoginActivity.java
@@ -21,6 +21,7 @@ public class LoginActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_login);
 
         txtEmail = (TextView) findViewById(R.id.editTextEmail);
